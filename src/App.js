b5b446b3fb0a17c8fe75f7f637b0a4646fcad4fa
Melinda Hardin <melindawad@gmail.com>
2018-06-08 19:54:19 -0500

    styles, some

--- src/App.js
+++ src/App.js
@@ -1,68 +1,97 @@
-import React from "react";
+import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import Wrapper from "./components/Wrapper/Wrapper";
 import Cards from "./components/cards/Cards";
 import friends from "./friends.json";
 import Footer from "./components/Footer";
 
-const App = () => (
-  <div>
-    <Navbar />
-    <Header />
 
-    <Cards
-      image={friends[0].image}
-    />
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends
+  };
 
-    <Cards
-      image={friends[1].image}
-    />
+  render() {
+    return (
+      <Wrapper>    
+        <Navbar />
+        <Header />
+         
+        {this.state.friends.map(friend => (
+          <Cards
+            image={friend.image}
+            
+          />
+        ))}
+      <Footer />
+      </Wrapper>
+    );
+  }
 
-    <Cards
-      image={friends[2].image}
-    />
 
-    <Cards
-      image={friends[3].image}
-    />
 
-    <Cards
-      image={friends[4].image}
-    />
+}
 
-    <Cards
-      image={friends[5].image}
-    />
+// const App = () => (
+//   <div>
+//     <Navbar />
+//     <Header />
 
-    <Cards
-      image={friends[6].image}
-    />
+//     <Cards
+//       image={friends[0].image}
+//     />
 
-    <Cards
-      image={friends[7].image}
-    />
+//     <Cards
+//       image={friends[1].image}
+//     />
 
-    <Cards
-      image={friends[8].image}
-    />
+//     <Cards
+//       image={friends[2].image}
+//     />
 
-    <Cards
-      image={friends[9].image}
-    />
+//     <Cards
+//       image={friends[3].image}
+//     />
 
-    <Cards
-      image={friends[10].image}
-    />
+//     <Cards
+//       image={friends[4].image}
+//     />
 
-    <Cards
-      image={friends[11].image}
-    />
+//     <Cards
+//       image={friends[5].image}
+//     />
+
+//     <Cards
+//       image={friends[6].image}
+//     />
+
+//     <Cards
+//       image={friends[7].image}
+//     />
+
+//     <Cards
+//       image={friends[8].image}
+//     />
+
+//     <Cards
+//       image={friends[9].image}
+//     />
+
+//     <Cards
+//       image={friends[10].image}
+//     />
+
+//     <Cards
+//       image={friends[11].image}
+//     />
 
   
 
-    <Footer />
-  </div>
-);
+//     <Footer />
+//   </div>
+// );
 
 export default App;
 
--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,24 +1,14 @@
 import React from "react";
+import "./Card.css";
 
-const styles = {
-  body: {
-    
-    float: "left"
-  }
-}
+
 
 const Cards = props => (
-
-  <div style={styles.body}>
-    <div className="card">
-      <div className="img-container">
-        <img alt={props.name} src={props.image} />
-      </div>
-      
+<div className="card">
+    <div className="img-container">
+      <img alt={props.name} src={props.image} />
     </div>
-
   </div>
-
 
 );
 
